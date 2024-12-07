de77c3311efe536cc53eabca312f17a75715548b
vantage-ola <vantageola4@gmail.com>
2024-12-07 01:20:19 +0100

    improve-mix-panel-fcp and its various impors across the components and pages

--- lib/utils/analytics.ts
+++ lib/utils/analytics.ts
@@ -1,15 +1,26 @@
 import mixpanel from 'mixpanel-browser';
 
-export const init = () => {
-  if (process.env.NEXT_PUBLIC_MIXPANEL_API_KEY) {
-    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_API_KEY, { ip: false });
-  }
-};
+export const analytics = {
+  isInitialized: false,
 
-export const track = (eventName: string, eventProperties: any) => {
-  if (typeof window === 'undefined') return;
+  // init only when first used
+  init() {
+    if (this.isInitialized) return;
 
-  if (process.env.NEXT_PUBLIC_MIXPANEL_API_KEY) {
+    const apiKey = process.env.NEXT_PUBLIC_MIXPANEL_API_KEY;
+    if (apiKey && typeof window !== 'undefined') {
+      mixpanel.init(apiKey, { ip: false });
+      this.isInitialized = true;
+    }
+  },
+
+  track(eventName: string, eventProperties?: Record<string, any>) {
+    if (typeof window === 'undefined' || !process.env.NEXT_PUBLIC_MIXPANEL_API_KEY) return;
+    // lazy initialize if not already done
+    if (!this.isInitialized) {
+      this.init();
+    }
+
     mixpanel.track(eventName, eventProperties);
-  }
+  },
 };
